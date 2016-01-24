ddbb1a1143512087564757831c22c068e85f7787
Sun Lijun <ashther@qq.com>
2016-01-24 19:58:39 +0800

    确定了自由度的并行算法

--- dictionary_create.R
+++ dictionary_create.R
@@ -2,17 +2,11 @@ library(jiebaR)
 library(dplyr)
 library(parallel)
 
-entropyCaculate_t <- function(vec) {
-    p <- vec / sum(vec)
-    sum(-p * log(p))
-}
-
 cl <- makeCluster(4)
 
 words <- '吃葡萄不吐葡萄皮不吃葡萄倒吐葡萄皮'
 
 n <- nchar(words)
-#words <- strsplit(test, '') %>% unlist() %>% unique()
 
 st <- 1:n
 
@@ -25,21 +19,24 @@ system.time(seg_words <- parSapply(cl, 0:4, function(i){
 
 system.time(word_dic <- table(unlist(seg_words)))
 rm(seg_words)
-word_dic <- word_dic / sum(word_dic)
+word_dic <- word_dic / n
+dic_names <- names(word_dic)
 
-system.time(pre_words <- parSapply(cl, st, function(i){
-    substring(words, i - 1, i - 1)
-}))
-pre_words <- replicate(5, pre_words)
+clusterExport(cl, 'dic_names')
+clusterEvalQ(cl, library(magrittr))
+clusterEvalQ(cl, entropyCaculate_t <- function(vec) {
+    vec <- table(vec)
+    p <- vec / sum(vec)
+    sum(-p * log(p))
+})
 
-system.time(suff_words <- parSapply(cl, 0:4, function(i){
-    substring(words, st + i + 1, st + i + 1)
+system.time(free <- parSapply(cl, dic_names, function(w){
+    pre <- gregexpr(w, words, fixed = TRUE)[[1]] %>% `-`(1) %>% substring(words, ., .) %>% entropyCaculate_t()
+    suff <- gregexpr(w, words, fixed = TRUE)[[1]] %>% `+`(nchar(w)) %>% substring(words, ., .) %>% entropyCaculate_t()
+    return(min(pre, suff))
 }))
+
 # ================================================================
-
-
-
-
 
 clusterEvalQ(cl, require(magrittr))
 clusterExport(cl, 'word_dic')
@@ -59,46 +56,7 @@ system.time(test <- parSapply(cl, names(word_dic), function(w){
     }
 }))
 
-# word_list <- vector('list', length = length(word_dic))
-# names(word_list) <- names(word_dic)
-# for (i in names(word_list)) {
-#     word_list[[i]] <- unname(word_dic[i]) / n
-# }
-# rm(word_dic)
-
-word_cohesion <- vector('list', length = length(word_dic))
-names(word_cohesion) <- names(word_dic)
-
-word_freedom <- vector('list', length = length(word_dic))
-names(word_freedom) <- names(word_dic)
-
-pb <- txtProgressBar(max = length(names(word_freedom)), style = 3)
-for (i in names(word_freedom)) {
-    temp <- gregexpr(i, words)[[1]]
-    pre_word <- substring(words, temp - 1, temp - 1) %>% 
-        table() %>% entropyCaculate_t()
-    suff_word <- substring(words, temp + nchar(i), temp + nchar(i)) %>% 
-        table() %>% entropyCaculate_t()
-    word_freedom[[i]] <- min(pre_word, suff_word)
-    
-    left <- 1:(nchar(i) - 1)
-    right <- rev(left)
-    left <- substring(i, 1, left)
-    right <- substring(i, nchar(i) - right + 1, nchar(i))
-    
-    if (nchar(i) == 1) {
-        word_cohesion[[i]] <- 0 # single word mutual information is 1?
-    } else {
-        word_cohesion[[i]] <- mapply(function(x, y){
-            log(word_dic[i] / (word_dic[x] * word_dic[y]))
-        }, left, right) %>% min()
-    }
-    
-    setTxtProgressBar(pb, value = which(i == names(word_freedom)))
-}
-close(pb)
-
-
+# ================================================================
 cohesion <- do.call('rbind', word_cohesion)
 cohesion <- data.frame(word = rownames(cohesion), cohesion, row.names = NULL) %>% 
     arrange(desc(cohesion))
